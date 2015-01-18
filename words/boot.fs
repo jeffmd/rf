29ba0c3855df4c1c490a8f797e3fa526ef73b787
nfz <shelleydoyle@eastlink.ca>
2015-01-18 00:09:00 -0400

    - added file boot.fs which contains boot strap words to build compiler

---
+++ words/boot.fs
@@ -0,0 +1,14 @@
+pname (create) current @ header
+smudge ! dp latest ! ]
+  pname current @ header
+[ ;opt uwid
+
+(create) :
+smudge ! dp latest ! ]
+  (create) smudge ! dp latest ! ]
+[ ;opt uwid
+
+
+: cur@
+  current @
+[ ;opt uwid
