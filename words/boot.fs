ac578dfde6b7b175c36a79553b3f4049b6c9ff16
nfz <shelleydoyle@eastlink.ca>
2015-01-18 01:01:54 -0400

    - added ldc32 lit - added forth source: widf immediate \ ; - NOTE: literal constants not compiling correctly yet

--- words/boot.fs
+++ words/boot.fs
@@ -8,7 +8,41 @@ smudge ! dp latest ! ]
   (create) smudge ! dp latest ! ]
 [ ;opt uwid
 
-
 : cur@
   current @
 [ ;opt uwid
+
+: widf
+    cur@
+    @
+    dup
+    h@
+    rot and
+    swap
+    h!
+[ ;opt uwid
+
+: immediate
+    $7FFF widf
+[ ;opt uwid immediate
+
+: \
+    stib
+    nip
+    >in
+    h!
+[ ;opt uwid immediate
+
+\ boot.fs - bootstrap the forth compiler
+\ (create) is created manually
+\ use (create) to make : then define the rest manually
+\ : can now be used to define a new word but must manually
+\ terminate the definition of a new word
+
+\ define ; which is used when finishing the compiling of a word
+: ;
+  \ change to interpret mode and override to compile [
+  [ pname [ findw nfa>xtf cxt ]
+  \ back in compile mode
+  ;opt uwid
+[ ;opt uwid immediate
