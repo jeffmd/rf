a319d984c3a72a56887d2a1aba6875cefee9efff
nfz <shelleydoyle@eastlink.ca>
2015-03-01 16:28:53 -0400

    - rearranged gpio word parameters to reduce stack juggling

--- words/gpio.fs
+++ words/gpio.fs
@@ -7,27 +7,27 @@ GPIO definitions
 \ automatically exports the pin 
 \ if direction is 0 then pin will be input
 \ if direction is 1 then pin will be output
-: pin ( direction pin -- )
-  dup gpio.x gpio.dir
+: pin ( pin direction -- )
+  over gpio.x gpio.dir
 ;
 
 \ set pin as output
 : out ( pin -- )
-  1 swap pin
+  1 pin
 ;
 
 \ set pin as input
 : in ( pin -- )
-  0 swap pin
+  0 pin
 ;
 
 \ turn gpio pin to on state
 : on ( pin -- )
-  1 swap gpio.write
+  1 gpio.write
 ;
 
 \ turn gpio pin to off state
 : off ( pin -- )
-  0 swap gpio.write
+  0 gpio.write
 ;
 
