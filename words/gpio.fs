a16dc56e149d7007435bbf13f9db82d91c806776
nfz <shelleydoyle@eastlink.ca>
2015-03-04 14:58:50 -0400

    - added #>Open as helper for opening sys gpio files - added .fail .wrting print helpers

--- words/gpio.fs
+++ words/gpio.fs
@@ -10,24 +10,29 @@ GPIO definitions
 : value$ s" /value" ;
 : gpio$ s" gpio" ;
 : inout$ s" inout" ;
-: fail$ s" Failed to open GPIO: " ;
 : inbuf$ s"    " ;
+
+: .fail ." Failed to open GPIO: " ;
+: .wrting ."  for writing!" ;
+
+: #>Open ( -- flag)
+    #$ root$ #$ #> drop open ( pin fs )
+    dup 0> ( pin fs flag)
+;
 
 \ unexport a GPIO pin when no longer needed by user
 : unexp ( pin --  )
     \ open unexport file for writing
     1    ( pin 1 )
     dup  ( pin 1 1 )
-    <# 0 hold unexp$ #$ 
-    root$ #$ #> drop open ( pin fs )
-    dup 0> ( pin fs flag)
+    <# 0 hold unexp$ #>Open ( pin fs flag)
     if
       dup ( pin fs fs )
       rot ( fs fs pin )
       <# #s #> write ( fs flag )
       swap
     else
-      fail$ type ." unexport for writing!"
+      .fail ." unexport" .wrting
     then
     nip
     close
@@ -41,16 +46,14 @@ GPIO definitions
     \ open direction file for writing
     1    ( pin direction 1 )
     rot  ( direction 1 pin )
-    <# 0 hold dir$ #$ #s gpio$ #$
-    root$ #$ #> drop open ( direction fs )
-    dup 0> ( direction fs flag)
+    <# 0 hold dir$ #$ #s gpio$ #>Open ( direction fs flag)
     if
       dup ( direction fs fs )
       rot ( fs fs direction )
       <# dup 2* inout$ drop + over 2+ #$ #> write ( fs flag )
       swap
     else
-      fail$ type ." direction for writing!"
+      .fail ." direction" .wrting
     then
     nip
     close
@@ -61,16 +64,14 @@ GPIO definitions
     \ open value file for writing
     1    ( pin val 1 )
     rot  ( val 1 pin )
-    <# 0 hold value$ #$ #s gpio$ #$
-    root$ #$ #> drop open ( val fs )
-    dup 0> ( val fs flag)
+    <# 0 hold value$ #$ #s gpio$ #>Open ( val fs flag)
     if
       dup ( val fs fs )
       rot ( fs fs val )
       <# #s #> write ( fs 1 )
       swap
     else
-      fail$ type ." value for writing!"
+      .fail ." value" .wrting
     then
     nip
     close
@@ -81,9 +82,7 @@ GPIO definitions
     \ open value file for reading
     0    ( pin 0 )
     swap ( 0 pin )
-    <# 0 hold value$ #$ #s gpio$ #$
-    root$ #$ #> drop open ( fs )
-    dup 0> ( fs flag)
+    <# 0 hold value$ #$ #s gpio$ #>Open ( fs flag)
     if
       dup ( fs fs )
       inbuf$ read ( fs flag )
@@ -92,7 +91,7 @@ GPIO definitions
       -rot ( val fs flag )
       swap
     else
-      fail$ type ." value for reading!"
+      .fail ." value for reading!"
       tuck ( flag fs flag )
     then
     nip
@@ -117,13 +116,13 @@ GPIO definitions
   0 pin
 ;
 
-\ turn gpio pin to on state
-: on ( pin -- )
+\ turn gpio pin to high state
+: high ( pin -- )
   1 gpiow
 ;
 
-\ turn gpio pin to off state
-: off ( pin -- )
+\ turn gpio pin to low state
+: low ( pin -- )
   0 gpiow
 ;
 
