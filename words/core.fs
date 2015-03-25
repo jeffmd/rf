8cbc60de8792cade76c80b2cd0c033d05650e282
nfz <shelleydoyle@eastlink.ca>
2015-03-24 21:01:35 -0300

    - added assembly words: lsl#, lsrs#, asrs#, - renamed assembly words that use an immediate value, use # instead of i in name to indicate an immediate - moved some words defined in core.S to core.fs: 2* 2/ 4* 4/ 2nip - using bxlr, now calls clrcache to keep instruction cache in sync with data cache when defining new words using assembly

--- words/core.fs
+++ words/core.fs
@@ -43,89 +43,120 @@
 
 \ inlinned assembly routines
 
+
+( n1 n2 n3 -- n3)
+\ drop NOS twice, two cells before TOS.
+rword 2nip inlined
+    8 dsp adds#,
+    bxlr,
+
+( n1 -- n1*2 )
+\ shift left to give * 2
+rword 2* inlined
+    tos tos 1 lsls#,
+    bxlr,
+
+( n1 -- n1/2 )
+\ arithmetic shift right to give / 2
+rword 2/ inlined
+    tos tos 1 asrs#,
+    bxlr,
+
+( n1 -- n1*4 )
+\ shift left to give * 4
+rword 4* inlined
+    tos tos 2 lsls#,
+    bxlr,
+
+( n1 -- n1/4 )
+\ shift right to give / 4
+rword 4/ inlined
+    tos tos 2 asrs#,
+    bxlr,
+
 ( n -- n+1 )
 \ optimized 1 increment
 rword 1+ inlined
-  1 tos addi,
-  bxlr,
+    1 tos adds#,
+    bxlr,
 
 ( n -- n-1 )
 \ optimized 1 decrement
 rword 1- inlined
-  1 tos subi,
-  bxlr,
+    1 tos subs#,
+    bxlr,
 
 ( n -- n+2 )
 \ optimized 2 increment
 rword 2+ inlined
-  2 tos addi,
-  bxlr,
+    2 tos adds#,
+    bxlr,
 
 ( n -- n-2 )
 \ optimized 2 decrement
 rword 2- inlined
-  2 tos subi,
-  bxlr,
+    2 tos subs#,
+    bxlr,
 
 ( n -- n+4 )
 \ optimized 4 increment
 rword 4+ inlined
-  4 tos addi,
-  bxlr,
+    4 tos adds#,
+    bxlr,
 
 ( n -- n-4 )
 \ optimized 4 decrement
 rword 4- inlined
-  4 tos subi,
-  bxlr,
+    4 tos subs#,
+    bxlr,
 
 ( -- icell )
 \ push instruction cell size 
 rword icell inlined
-  ] 2 [
-  bxlr,
+    ] 2 [
+    bxlr,
 
 ( n -- n+icell )
 \ add instruction stack cell size to n
 rword icell+ inlined
-  ] 2+ [
-  bxlr,
+    ] 2+ [
+    bxlr,
   
 ( n -- n-icell )
 \ subtract instruction stack cell size from n
 rword icell- inlined
-  ] 2- [
-  bxlr,
+    ] 2- [
+    bxlr,
 
 ( n -- n*icell )
 \ multiply n by instruction stack cell size 
 rword icell* inlined
-  ] 2* [
-  bxlr,
+    ] 2* [
+    bxlr,
 
 ( -- dcell )
 \ push data stack cell size 
 rword dcell inlined
-  ] 4 [
-  bxlr,
+    ] 4 [
+    bxlr,
 
 ( n -- n+dcell )
 \ add data stack cell size to n
 rword dcell+ inlined
-  ] 4+ [
-  bxlr,
+    ] 4+ [
+    bxlr,
 
 ( n -- n-dcell )
 \ subtract data stack cell size from n
 rword dcell- inlined
-  ] 4- [
-  bxlr,
+    ] 4- [
+    bxlr,
 
 ( n -- n*dcell )
 \ multiply n by data stack cell size 
 rword dcell* inlined
-  ] 4* [
-  bxlr,
+    ] 4* [
+    bxlr,
 
 \ search dictionary for name, returns XT or 0
 : 'f  ( "<spaces>name" -- XT XTflags )
